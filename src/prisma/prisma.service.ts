0b54ef4f85649b6b9de33935bfd78568ab3727b6
shlokstack <shlokshah3112006@gmail.com>
2026-09-05 19:05:48 +0530

    Initial backend implementation

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,20 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import postgres from '@prisma/orm-postgres/runtime';
 
 import type { Contract } from '../../prisma/contract.d.ts';
 import contractJson from '../../prisma/contract.json' with { type: 'json' };
 
 @Injectable()
-export class PrismaService implements OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
   readonly db = postgres<Contract>({
     contractJson,
-    url: process.env['DATABASE_URL']!,
   });
+
+  async onModuleInit() {
+    await this.db.connect({
+      url: process.env['DATABASE_URL']!,
+    });
+  }
 
   async onModuleDestroy() {
     await this.db.close();
